c500e768f103e5c1a498a948c21d26473f425bcb
eduroses <edu.s.roses@gmail.com>
2026-08-27 12:03:49 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='calistenia-militar-final-v3';
-const ASSETS=['./','./index.html','./manifest.webmanifest'];
+const CACHE='calistenia-militar-final-v4';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./assets/exercises/flexiones-de-pecho.jpg','./assets/exercises/sentadillas.jpg','./assets/exercises/remo-invertido.jpg','./assets/exercises/fondos-en-silla.jpg','./assets/exercises/plancha.jpg','./assets/exercises/superman.jpg','./assets/exercises/mountain-climbers.jpg','./assets/exercises/burpees.jpg','./assets/exercises/plancha-lateral.jpg','./assets/exercises/abdominal-bicicleta.jpg','./assets/exercises/referencia-general.jpg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});

d9545b20d5bd0d454ea000b7e20902de0ef73033
eduroses <edu.s.roses@gmail.com>
2026-08-27 10:24:15 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "calistenia-8w-final-v1";
+const CACHE = "calistenia-8w-final-v2";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
